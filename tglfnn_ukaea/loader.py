f4840d889183a483def839360323028e58afeb24
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2026-06-15 09:56:59 +0100

    Expose STEP networks in Python interface

--- tglfnn_ukaea/loader.py
+++ tglfnn_ukaea/loader.py
@@ -3,22 +3,15 @@ import pickle
 from typing import Any, Literal, Mapping
 
 
-def load(machine: Literal["step", "multimachine"] = "multimachine") -> Mapping[str, Any]:
-    if machine == "step":
-        pickle_file = pathlib.Path(__file__).parent / "weights" / "step.pkl"
-        if not pickle_file.exists():
-            raise FileNotFoundError(
-                f"Pickle file for 'step' version of tglfnn-ukaea not found at {pickle_file}."
-                " This may be because you installed the public version of the package, which"
-                " does not include the 'step' model weights due to licensing restrictions."
-            )
-    elif machine == "multimachine":
-        pickle_file = pathlib.Path(__file__).parent / "weights" / "multimachine.pkl"
-    else:
+def load(
+    machine: Literal["step", "multimachine"] = "multimachine",
+) -> Mapping[str, Any]:
+    if machine not in ["step", "multimachine"]:
         raise ValueError(
             f"Unknown machine type: '{machine}' (must be 'step' or 'multimachine')"
         )
 
+    pickle_file = pathlib.Path(__file__).parent / "weights" / f"{machine}.pkl"
     with open(pickle_file, "rb") as f:
         data = pickle.load(f)
 
